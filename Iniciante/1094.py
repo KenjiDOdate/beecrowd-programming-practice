a54a4807d249b99d774e671f98a13442a3228691
KenjiDOdate <kdodate@id.uff.br>
2026-03-03 12:17:45 -0300

    Fix 1094

--- Iniciante/1094.py
+++ Iniciante/1094.py
@@ -1,34 +1,23 @@
-def porcentagem(total, subtotal):
-    if total == 0:
-        return total
-    return (subtotal/total)*100
+def percentual(tipo, total):
+    calculo = (cobaias[tipo]/total) * 100
+    print(f"Percentual de {tipoCobaia[tipo]}: {calculo:.2f} %")
+    
 
 n = int(input())
 
-total = 0
-tcoelho = 0 
-trato = 0
-tsapo = 0
+cobaias = {'C': 0, 'R': 0, 'S':0}
+tipoCobaia = {'C': 'coelhos', 'R': 'ratos', 'S': 'sapos'}
 
-for i in range(n):
-    qtd, subject = input().split()
-    qtd = int(qtd)
-    total += qtd
-    if subject == "C":
-        tcoelho += qtd
-    elif subject == "R":
-        trato +=qtd
-    elif subject == "S":
-        tsapo += qtd
+for _ in range(n):
+   qtd, tipo = input().split()
+   cobaias[tipo] += int(qtd)
 
-centcoelho = porcentagem(total, tcoelho)
-centrato = porcentagem(total, trato)
-centsapo = porcentagem(total, tsapo)
+total = sum(cobaias.values())
 
 print(f"Total: {total} cobaias")
-print(f"Total de coelhos: {tcoelho}")
-print(f"Total de ratos: {trato}")
-print(f"Total de sapos: 23")
-print(f"Percentual de coelhos: {centcoelho:.2f} %")
-print(f"Percentual de ratos: {centrato:.2f} %")
-print(f"Percentual de sapos: {centsapo:.2f} %")
+for tipo in cobaias:
+    print(f"Total de {tipoCobaia[tipo]}: {cobaias[tipo]}")
+
+
+for tipo in cobaias:
+    percentual(tipo, total)
